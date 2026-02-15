a5ac7d72d14f3862b69596ca4b9cd698f2710209
Harsh Karan <harshkaranofficial@gmail.com>
2026-02-15 21:50:13 +0530

    Functions

---
+++ 01_Basics/05_Functions/01_BasicFunction.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+void fun(){
+    cout<<"Hello :)"<<endl;
+    cout<<2+3;
+}
+
+int main(){
+    fun();
+}
---
+++ 01_Basics/05_Functions/02_ReturnType.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int mini(int x , int y){
+    int a;
+    if(x>y) a=y;
+    else a = x;
+    return a;
+}
+int main(){
+    int x ,y;
+cin>>x>>y;
+cout<<mini(x,y);
+}
