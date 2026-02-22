8028df7e9f97df78a5e1b0b4b20446deba88c11e
Harsh Karan <harshkaranofficial@gmail.com>
2026-02-22 16:19:33 +0530

    Trapping Rain Water (Leetcode-42)

---
+++ 02_Arrays/35_TrappingWater2.cpp
@@ -0,0 +1,50 @@
+#include<iostream>
+#include<vector>
+#include<algorithm>
+using namespace std;
+
+    int trap(vector<int>& height) {
+        int n = height.size();
+        // Previous Greatest Element
+        vector<int> pre(n);
+        pre[0] = -1;
+        int max = height[0];
+        for(int i=1 ; i<=n-1; i++){
+            pre[i] = max;
+            if(max<height[i]) max = height[i];
+        } 
+
+        // Next Greatest Element
+        vector<int> next(n);
+        next[n-1] = -1;
+        max = height[n-1];
+        for(int i=n-2; i>=0; i--){
+            next[i] = max;
+            if(max<height[i]) max = height[i];
+        }
+
+        //minimum array -> prev as minimum
+        for(int i=0; i<n; i++){
+            if(pre[i]>=next[i]) pre[i] = next[i];
+        }
+
+        //Calculate Water
+        int water = 0;
+        for(int i=1; i<n-1; i++){
+            if(pre[i]>height[i]){
+                water += (pre[i]-height[i]);
+            }
+        }
+    return water;
+    }
+int main(){
+    vector<int> v;
+    v.push_back(4);
+    v.push_back(2);
+    v.push_back(0);
+    v.push_back(3);
+    v.push_back(2);
+    v.push_back(5);
+    cout<<trap(v);
+    
+}
