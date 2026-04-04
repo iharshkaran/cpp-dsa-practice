0736ebe4170b1919c78e586e2ba9298b6595841c
HARSH <harshkaranofficial@gmail.com>
2026-04-04 14:38:47 +0530

    Power Function (Logarithmic)

--- 07_Recursion/08_PowLog.cpp
+++ 07_Recursion/08_PowLog.cpp
@@ -17,28 +17,3 @@ int power(int x, int n){
 int main(){
     cout<<power(3,6);
 }
-
-
-
-
-
-// leetCode - 50 (power(x,n))
-class Solution {
-public:
-    double myPow(double x, int n) {
-        long long N = n;   // avoid overflow 
-        if (N < 0) { // -ve power
-            x = 1 / x;
-            N = -N;
-        }
-        double ans = 1;
-        while (N > 0) { // +ve power
-            if (N % 2 == 1) {
-                ans *= x;
-            }
-            x *= x;
-            N /= 2;
-        }
-        return ans;
-    }
-};
