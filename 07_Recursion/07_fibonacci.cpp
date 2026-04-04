f6aba37ae7fe0a178d3a92770116401e92506dfe
HARSH <harshkaranofficial@gmail.com>
2026-04-04 14:09:30 +0530

    Fibonacci

--- 07_Recursion/07_fibonacci.cpp
+++ 07_Recursion/07_fibonacci.cpp
@@ -2,7 +2,10 @@
 using namespace std;
 int fibonacci(int n){
     if(n==1 || n==2) return 1;
-    return fibonacci(n-1)+fibonacci(n-2);
+    int leftAns = fibonacci(n-1);
+    int rightAns = fibonacci(n-1);
+    return leftAns + rightAns;
+    // return fibonacci(n-1)+fibonacci(n-2);
 }
     
 int main(){
