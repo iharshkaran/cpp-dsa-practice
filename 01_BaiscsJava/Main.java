40ed7197e8c08be61eb590a57791ee848eaf301d
HARSH <harshkaranofficial@gmail.com>
2026-05-01 22:02:28 +0530

    Try Java

---
+++ 01_BaiscsJava/Main.java
@@ -0,0 +1,13 @@
+class LoopExample{
+    void start(){
+        for (int i = 1; i <= 5; i++) {
+            IO.println("i = " + i);
+        }
+    }
+
+
+}
+void main() {
+    LoopExample l = new LoopExample();
+    l.start();
+}
